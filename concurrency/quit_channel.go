453875e1fdae9fe7c243553149e9202c4c0c7de2
ALBERTO TORRES <scyanh@gmail.com>
2022-09-04 15:08:29 -0500

    add quit channel pattern

---
+++ concurrency/quit_channel.go
@@ -0,0 +1,36 @@
+package concurrency
+
+import "fmt"
+
+type quitChannel struct {
+}
+
+func NewQuitChannel() quitChannel {
+	return quitChannel{}
+}
+
+func (quitChannel) DoRace(idx int, racer, quit chan int) {
+	racer <- idx
+	// do something
+	quit <- idx
+}
+
+func (q quitChannel) Racing() {
+	defer fmt.Println("done")
+	c := make(chan int)
+	quit := make(chan int)
+
+	for i := 0; i < 3; i++ {
+		go q.DoRace(i, c, quit)
+	}
+
+	for {
+		select {
+		case racer := <-c:
+			fmt.Println(racer)
+		case finished := <-quit:
+			fmt.Println("quit by ", finished)
+			return
+		}
+	}
+}
